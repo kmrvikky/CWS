0f62d3bc9a3dedb8ae82a239435bb321364dd0bf
Kr Vikky <127074056+kmrvikky@users.noreply.github.com>
2025-01-15 23:17:30 +0530

    CWS_2.py
    CWS
    Day1 Task

---
+++ CWS_2.py
@@ -0,0 +1,8 @@
+# Q12. Write a Python program that converts temperatures from Celsius to Fahrenheit. Use the formula (Celsius * 9/5) + 32 to perform the conversion.
+
+C = int(input("Enter the temperature : "))
+
+F = (C * 9/5) + 32
+
+print ("The temperature in Fahrenheit is =",F)
+
