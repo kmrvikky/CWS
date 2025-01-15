c31a001818002a65b3b89ec1223f9fc54b0bb1f8
Kr Vikky <127074056+kmrvikky@users.noreply.github.com>
2025-01-15 23:09:06 +0530

    CWS_1.py
    Day1
    Tasks

---
+++ CWS_1.py
@@ -0,0 +1,11 @@
+# Coding Challenges
+
+# Q11. Develop a Python script to calculate the area of a rectangle. Your program should take the rectangle's length and width, and then display the calculated area.
+
+l = int(input("enter the length = "))
+w = int(input("enter the width = "))
+
+# area = (l*w)
+
+print ("The area of the rectangle =",(l*w),"sqr unit")
+
